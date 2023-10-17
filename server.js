76b3253f970d0ca9d69e607871352b49b13ba2bb
Maddie <maddieeckstrom@gmail.com>
2023-10-17 10:58:13 -1000

    added comments to server.js file

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ const util = require('util');
 app.use(express.json());
 app.use(express.static('public'));
 
+// a route path which listens for a get request from the /api/notes and responds with db.json
 app.get('/api/notes', (req, res) => {
     res.json(notesDb);
 });
 
+// a route path which listens for a post request from /api/notes, and responds with a json including the recently pushed and already existing notes
 app.post('/api/notes', (req, res) => {
     
     let jsonFilePath= path.join(__dirname, "db/db.json");
@@ -34,12 +36,15 @@ app.post('/api/notes', (req, res) => {
     res.json(newNote);
 })
 
+// a route path which listens for a get request from /notes and responds with sending the notes.html file
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/notes.html'));
 })
 
+// a route path which listens for a get request from a catch-all, and responds with sending the index.html file
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
+// If the server connects when prompted, successfully connected will log
 app.listen(PORT, () => console.log('Successfully connected'));
